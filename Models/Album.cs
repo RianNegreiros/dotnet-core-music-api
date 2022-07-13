12802bb8c5a3ff30214cfa95ffcddb072e1a7988
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-13 13:52:13 -0300

    add models

---
+++ Models/Album.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace musicApi.Models;
+
+public class Album
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [NotMapped]
+    public IFormFile Image { get; set; }
+    public string ImageUrl { get; set; }
+    public int ArtistId { get; set; }
+    public ICollection<Song> Songs { get; set; }
+}
---
+++ Models/Artist.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace musicApi.Models;
+
+public class Artist
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [NotMapped]
+    public IFormFile Image { get; set; }
+    public string ImageUrl { get; set; }
+    public ICollection<Album> Albums { get; set; }
+    public ICollection<Song> Songs { get; set; }
+}
