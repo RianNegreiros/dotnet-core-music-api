3a379ef102dfcf9c43fdb147b2eddb71994c46a3
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-13 17:33:36 -0300

    add  database config

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using musicApi.Models;
+
 namespace MusicApi.Data;
 
-public class DataContext
+public class DataContext : DbContext
 {
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+    }
     
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Album> Albums { get; set; }
+    public DbSet<Song> Songs { get; set; }
 }
